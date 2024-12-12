a229fac4e79022ba22ff7f2c0a2f0f39b213b226
spark0698 <sean91827364@gmail.com>
2024-12-12 15:26:34 -0800

    adding new refactored files

---
+++ transformations/schemas.py
@@ -0,0 +1,31 @@
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DecimalType, TimestampNTZType, BinaryType
+
+fire_schema = StructType([
+    StructField('address', StringType(), False),
+    StructField('type', StringType(), False),
+    StructField('datetime', TimestampNTZType(), False),
+    StructField('latitude', DecimalType(25, 20), False),
+    StructField('longitude', DecimalType(25, 20), False),
+    StructField('report_location', StringType(), False),
+    StructField('incident_number', StringType(), False)
+])
+
+crime_schema = StructType([
+    StructField('report_number', StringType(), False),
+    StructField('offense_id', StringType(), False),
+    StructField('offense_start_datetime', TimestampNTZType(), False),
+    StructField('report_datetime', TimestampNTZType(), False),
+    StructField('group_a_b', StringType(), False),
+    StructField('crime_against_category', StringType(), False),
+    StructField('offense_parent_group', StringType(), False),
+    StructField('offense', StringType(), False),
+    StructField('offense_code', StringType(), False),
+    StructField('precinct', StringType(), False),
+    StructField('sector', StringType(), False),
+    StructField('beat', StringType(), False),
+    StructField('mcpp', StringType(), False),
+    StructField('_100_block_address', StringType(), False),
+    StructField('longitude', DecimalType(25, 20), False),
+    StructField('latitude', DecimalType(25, 20), False),
+    StructField('offense_end_datetime', TimestampNTZType(), False)
+])
